928fdcac7998bc882aec10177c913ab560edf103
Vladimir Mustafin <vova.must@gmail.com>
2020-03-20 20:47:52 +0700

    Correct exercise 6

--- Module4.playground/Contents.swift
+++ Module4.playground/Contents.swift
@@ -14,31 +14,34 @@ class gameCardControl{
     */
 class Position{
     
-    var m_xPos:UInt
-    var m_yPos:UInt
     
     init(x:UInt,y:UInt) {
         m_xPos=x
         m_yPos=y
     }
+    
+    var m_xPos:UInt
+    var m_yPos:UInt
 }
 class Circle {
-    var m_pos: Position
-    var m_radius:UInt
     init(x_pos:UInt,y_pos:UInt,radius:UInt) {
         m_pos=Position( x: x_pos, y: y_pos)
         m_radius=radius
     }
+    
+    var m_pos: Position
+    var m_radius:UInt
 }
 class Rectangle {
-    var m_pos: Position
-    var m_width:UInt
-    var m_height:UInt
     init(x_pos:UInt,y_pos:UInt,width:UInt,height:UInt) {
         m_pos=Position( x: x_pos, y: y_pos)
         m_width=width
         m_height=height
     }
+    
+    var m_pos: Position
+    var m_width:UInt
+    var m_height:UInt
 }
 
 /* Exersice 5
@@ -46,3 +49,20 @@ class Rectangle {
         В 3 и 4 случае, т.к. структуры константные
         В 5 так же, как и в 1
         В 7 тк user.name константная переменная */
+
+/* Exersice 6 */
+class Car {
+    init(carName:String, engineModel:String, horsePower:UInt = 120, peopleCapacity:UInt = 5, carType:String = "Sedan") {
+        m_carName = carName
+        m_engineModel = engineModel
+        m_horsePower = horsePower
+        m_peopleCapacity = peopleCapacity
+        m_carType = carType
+    }
+    var m_carName:String
+    var m_engineModel:String
+    var m_horsePower:UInt
+    var m_peopleCapacity:UInt
+    var m_carType:String
+}
+
