7683e972635624eb74b93551c478d7e955c692eb
Vladimir Mustafin <vova.must@gmail.com>
2020-03-24 23:48:05 +0700

    Redid the exercise

--- BonusForModule3.playground/Contents.swift
+++ BonusForModule3.playground/Contents.swift
@@ -1,20 +1,11 @@
 import UIKit
 
-/* Exersice 1 */
-
-struct User {
-    let hasLogin = false
-    let hasPassword = false
-}
-
-func shouldShowLoginButton(for user: User) -> Bool {
-    return user.hasPassword && user.hasLogin ? true : false
-}
 
 /* Exersice 2 */
-let names = ["Vladimir", "Dmitriy", "Nikolay", "Petr"]
-names.forEach{ print("\($0); ", terminator:"") }
 
+let names = ["Vladimir", "Dmitriy", "Nikolay", "Petr"]
+print(names.reduce("",{x,y in x+y+";"})) //тут все равно в конце ";" ((
+print(names.joined(separator: ";"))
 /* Exersice 3 */
 
 let numbers = [1, 2, 3, 4]
@@ -30,5 +21,14 @@ enum AutorizateType {
     case login(String)
 }
 
-/* Точно выбрал class, ибо это ссылочный тип и там реализован подсчет ссылок, очень удобно передавать ее всюду, а не копировать.*/
-/* Не совсем понял что значит реализовать "код структуры". Во первых он получится не универсальный, т.к. я пока не знаю как пользоваться template, указателей тут тоже я пока не знаю что бы организовывать детей и т.д., а вот например set & dictionary по сути и представляют собой дерево, только оно еще реализвованно на hashtable, супер быстрая штука. А обертывать одну из этих структур я что-то не понял, из задания, зачем.  */
+/* Exersice 5
+ Я глянул и все вами перечесленные контейнеры являются value type. Я думаю это связано с механикой inout, которая позвоялет более гибко управлять контейнерами. Но тогда я не понимаю почему вы согласились с моим утверждение про дерево и класс.
+ Судя по всему копируется.
+ */
+
+indirect enum treeNode {
+    case value(String,Any)
+    case parent(treeNode?)
+    case leftChild(treeNode?)
+    case rightChild(treeNode?)
+}
