71cd67f0aff999d12cb347569d0ea9a6f84b67a2
Vladimir <vova.must@gmail.com>
2020-08-16 16:58:56 +0700

    Correct .gitignore & create proj

---
+++ Standart UI/Standart UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Standart UI
+//
+//  Created by Vladimir Mustafin on 16.08.2020.
+//  Copyright © 2020 Vladimir Mustafin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
