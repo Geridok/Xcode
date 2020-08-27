82748de5fd806b88fa8a7649b48aa32b2f5d5e6d
Vladimir <vova.must@gmail.com>
2020-08-27 19:12:39 +0700

    Create Project

---
+++ Constraints/Constraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Vladimir Mustafin on 27.08.2020.
+//  Copyright © 2020 Vladimir Mustafin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
