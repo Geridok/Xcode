6bfdea760171bad8426e6a5170966e9a586d0e47
Vladimir <vova.must@gmail.com>
2020-09-23 20:25:07 +0700

    Create proj

---
+++ Tables&Collections/Tables&Collections/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tables&Collections
+//
+//  Created by Vladimir Mustafin on 23.09.2020.
+//  Copyright © 2020 Vladimir Mustafin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
