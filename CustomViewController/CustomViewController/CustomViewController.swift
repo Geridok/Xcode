4e1ae05b33f747815c1b398deb00cbaa0b902e83
Vladimir <vova.must@gmail.com>
2020-10-20 23:23:11 +0700

    Non complete

--- CustomViewController/CustomViewController/CustomViewController.swift
+++ CustomViewController/CustomViewController/CustomViewController.swift
@@ -10,109 +10,108 @@ import UIKit
 
 class CustomViewController: UIViewController {
     
-    @IBOutlet var buttonArray:[UIButton]!
+    var isHidden = [Bool](repeating: true, count: 6)
     
-    var UIImageArrayFill:[UIImage]? = ["1.circle.fill","2.circle.fill","3.circle.fill"].map { UIImage(systemName: $0) } as? [UIImage]
-    
-    var UIImageArray:[UIImage]? = ["1.circle","2.circle","3.circle"].map { UIImage(systemName: $0) } as? [UIImage]
-    
-    var viewControllerArray:[UIViewController] = []
-    
-    @IBOutlet weak var safeAreaForControllers: UIView!
-    
-    var rectForBound:CGRect?
-    var rectForTopView:CGRect?
-    var rectForBottomView:CGRect?
-    
-    var isFilling:[Bool] = []
-    var lastShowed:[Bool] = []
+     private var buttonsStackView: UIStackView?
+     private var childVCStackView: UIStackView?
+     
+    private var childs: [UIViewController] = []
+    private var childsButtons: [UIButton] = []
+     private var placeholderVC: UIViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let arr = UIImageArrayFill{
-            for _ in arr.enumerated() {
-                isFilling.append(false)
-                lastShowed.append(false)
+        
+        
+        
+        buttonsStackView = UIStackView()
+        buttonsStackView?.axis  = NSLayoutConstraint.Axis.horizontal
+        buttonsStackView?.distribution  = UIStackView.Distribution.fillEqually
+        buttonsStackView?.alignment = UIStackView.Alignment.fill
+        buttonsStackView?.spacing   = 10.0
+        buttonsStackView?.frame = CGRect(x: 0.0, y: 0.0, width: self.view.bounds.width , height: 75.0)
+        buttonsStackView?.bounds = CGRect(x: 0.0, y: 0.0, width: self.view.bounds.width , height: 75.0)
+        buttonsStackView?.backgroundColor = .green
+        buttonsStackView?.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        childVCStackView = UIStackView()
+        childVCStackView?.axis  = NSLayoutConstraint.Axis.vertical
+        childVCStackView?.distribution  = UIStackView.Distribution.fillEqually
+        childVCStackView?.alignment = UIStackView.Alignment.fill
+        childVCStackView?.spacing   = 0.0
+        childVCStackView?.frame = CGRect(x: 0.0, y: 75.0, width: self.view.bounds.width , height: self.view.bounds.height - 75.0)
+        childVCStackView?.bounds = CGRect(x: 0.0, y: 0.0, width: self.view.bounds.width , height: self.view.bounds.height - 75.0)
+        childVCStackView?.backgroundColor = .green
+        childVCStackView?.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        self.view.addSubview(buttonsStackView!)
+        self.view.addSubview(childVCStackView!)
+        buttonsStackView?.isHidden = false
+        childVCStackView?.isHidden = false
+        
+        
+    }
+     
+     func addVC(_ vc: UIViewController, buttonTitle: String) {
+         assert(childs.count < 6, "Too many child ViewControllers: only 6 allowed")
+        childs.append(vc)
+        vc.view.isHidden = true
+        
+        var button = UIButton()
+        button.titleLabel?.text = buttonTitle
+        childsButtons.append(button)
+        button.titleLabel?.textAlignment = .center
+        buttonsStackView?.addSubview(button)
+        button.isHidden = false
+        
+         // Сохраняем контроллер (но не показываем), создаем кнопку, показываем кнопку.
+     }
+     
+     func setPlaceholder(_ vc: UIViewController) {
+        placeholderVC = vc
+        self.view.addSubview(vc.view)
+        placeholderVC?.view.isHidden = true
+     }
+     
+    @objc private func showHideContentVC(_ sender: UIButton) {
+        if let index = childsButtons.firstIndex(of: sender){
+            if(isHidden[index]){
+                showChildVC(childs[index])
+            }else{
+                hideChildVC(childs[index])
             }
         }
-        let size = safeAreaForControllers.bounds.size
-        rectForTopView = CGRect(x: safeAreaForControllers.frame.origin.x, y: safeAreaForControllers.frame.origin.y, width: size.width, height: size.height/2)
-        rectForBottomView = CGRect(x: safeAreaForControllers.frame.origin.x, y: safeAreaForControllers.frame.origin.y + size.height/2, width: size.width, height: size.height/2)
-        rectForBound = CGRect(x: 0, y: 0, width: size.width, height: size.height/2)
-        viewControllerArray.append(createViewController(backgroundColor: .blue, bound: rectForBound!))
-        viewControllerArray.append(createViewController(backgroundColor: .red, bound: rectForBound!))
-        viewControllerArray.append(createViewController(backgroundColor: .yellow, bound: rectForBound!))
-        for item in viewControllerArray {
-            self.view.addSubview(item.view)
-            item.view.isHidden = true
+        if let index = isHidden.firstIndex(of: false){
+            
+        }else{
+            placeholderVC?.view.isHidden = false
         }
-    }
+         // Если все контент контроллеры скрыты, то показываем placeholder
+     }
 
-    @IBAction func viewChoice(_ sender: UIButton) {
-       
-        if let pressedButton = buttonArray.firstIndex(of: sender){
-            
-            //Here you should hide the stub
-            
-            if isFilling[pressedButton] {
-                sender.setImage(UIImageArray?[pressedButton], for: .normal)
-                isFilling[pressedButton] = false
-                viewControllerArray[pressedButton].view.isHidden = true
-                lastShowed[pressedButton] = false
-                if let fillButtonIndex = isFilling.firstIndex(of: true){
-                    lastShowed[fillButtonIndex] = true
-                }else{
-                    //You can specify a stub here
-                }
-            }else{
-                showController(pressedButton: pressedButton)
-                sender.setImage(UIImageArrayFill?[pressedButton], for: .normal)
-                isFilling[pressedButton] = true
-            }
-            }
+     private func showChildVC(_ childVC: UIViewController) {
+        placeholderVC?.view.isHidden = true
+        self.addChild(childVC)
+        childVC.didMove(toParent: self)
+        childVC.view.isHidden = false
+        childVCStackView?.addSubview(childVC.view)
+        if let index = childs.firstIndex(of: childVC){
+            isHidden[index] = false
         }
-        
-    private func createViewController( backgroundColor:UIColor, bound:CGRect)->UIViewController{
-        let newViewController = UIViewController()
-        newViewController.view.backgroundColor = backgroundColor
-        newViewController.view.bounds = bound
-        return newViewController
-    }
-    
-    private func showController(pressedButton: Array<Any>.Index){
-        var count = 0
-        for item in isFilling {
-            if item == true {
-                count += 1
-            }
+         // Функция для добавления контроллера в иерархию и его показа
+     }
+     
+     private func hideChildVC(_ childVC: UIViewController) {
+        childVC.willMove(toParent: self)
+        childVC.view.removeFromSuperview()
+        childVC.removeFromParent()
+        childVC.view.isHidden = true
+        if let index = childs.firstIndex(of: childVC){
+            isHidden[index] = true
         }
-            if let lastShowedIndex = lastShowed.firstIndex(of: true){
-                if(count == 2){
-                    if viewControllerArray[lastShowedIndex].view.frame == rectForTopView {
-                        viewControllerArray[pressedButton].view.frame = rectForTopView!
-                    }else{
-                        viewControllerArray[pressedButton].view.frame = rectForBottomView!
-                    }
-                    viewChoice(buttonArray[lastShowedIndex])
-                    lastShowed[pressedButton] = true
-                    viewControllerArray[pressedButton].view.isHidden = false
-                }else{
-                        if viewControllerArray[lastShowedIndex].view.frame == rectForTopView {
-                            viewControllerArray[pressedButton].view.frame = rectForBottomView!
-                        }else{
-                            viewControllerArray[pressedButton].view.frame = rectForTopView!
-                        }
-                    lastShowed[lastShowedIndex] = false
-                    lastShowed[pressedButton] = true
-                    viewControllerArray[pressedButton].view.isHidden = false
-                }
-            }else{
-                lastShowed[pressedButton] = true
-                viewControllerArray[pressedButton].view.frame = rectForTopView!
-                viewControllerArray[pressedButton].view.isHidden = false
-        }
-        
-    }
+     }
     
 }
 
