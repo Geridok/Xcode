f7a9931d7cebae6b222c14e222404f6d9236d854
Vladimir <vova.must@gmail.com>
2021-04-19 20:49:37 +0700

    Add new git ignore (for folder Realm)

---
+++ Module14DataStorage/Module14DataStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module14DataStorage
+//
+//  Created by Vladimir Mustafin on 19.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
