78c0ce5abe72f5bd14bb5db2097f7be8b4e317f8
Vladimir <vova.must@gmail.com>
2021-04-19 12:32:15 +0700

    Complete

--- StandartAnimation/StandartAnimation/ViewController.swift
+++ StandartAnimation/StandartAnimation/ViewController.swift
@@ -15,16 +15,39 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var animationIndexlabel: UILabel!
     
-    @IBOutlet weak var animatableView: UIView!
+    let animatableView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
     
-    let animationIndex = [0,1,2,3,4,5,6,7]
+    let animationIndex = [0,1,2,3,4,5]
+    var rightAnchor: NSLayoutConstraint?
+    var leftAnchor: NSLayoutConstraint?
+    var topAnchor: NSLayoutConstraint?
+    var widthAnchor: NSLayoutConstraint?
+    var heightAnchor: NSLayoutConstraint?
+    
+    private let kRotationAnimationKey = "rotationanimationkey"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(animatableView)
+        topAnchor = animatableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        topAnchor?.isActive = true
+        leftAnchor = animatableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor)
+        leftAnchor?.isActive = true
+        widthAnchor = animatableView.widthAnchor.constraint(equalToConstant: 100)
+        widthAnchor?.isActive = true
+        heightAnchor = animatableView.heightAnchor.constraint(equalToConstant: 100)
+        heightAnchor?.isActive = true
+        rightAnchor = animatableView.rightAnchor.constraint(equalTo: view.rightAnchor)
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        changeBackgroundColor(color: .yellow)
+        changeViewBackgroundColor(color: .yellow)
     }
     
     @IBAction func changeAnimation(_ sender: UIButton) {
@@ -50,27 +73,23 @@ class ViewController: UIViewController {
         
         switch currentIndex {
         case 0:
-            changeBackgroundColor(color: .yellow)
+            changeViewBackgroundColor(color: .yellow)
         case 1:
-            moveToRightPoint()
+            moveViewToRightPoint()
         case 2:
-            changeBackgroundColor(color: .yellow)
+            ViewToCircle(duration: 3.0)
         case 3:
-            changeBackgroundColor(color: .yellow)
+            rotateView(duration: 3.0)
         case 4:
-            changeBackgroundColor(color: .yellow)
+            viewDisappearance()
         case 5:
-            changeBackgroundColor(color: .yellow)
-        case 6:
-            changeBackgroundColor(color: .yellow)
-        case 7:
-            changeBackgroundColor(color: .yellow)
+            viewGrow()
         default:
             return
         }
     }
     
-    private func changeBackgroundColor(color: UIColor,duration: CGFloat = 0.5){
+    private func changeViewBackgroundColor(color: UIColor,duration: CGFloat = 0.5){
         UIView.animate(withDuration: TimeInterval(duration), delay: 0, options: .autoreverse, animations: {
             self.animatableView.backgroundColor = color
         }) { (isCompleted) in
@@ -80,16 +99,89 @@ class ViewController: UIViewController {
         }
     }
     
-    private func moveToRightPoint(duration: CGFloat = 1.5){
-        //self.animatableView.frame.origin.y = 44
-        UIView.animate(withDuration: TimeInterval(duration), delay: 0.5, animations: {
-            self.animatableView.frame.origin.y = 400
-        }) { (isCompleted) in
-            if(isCompleted){
-                self.animatableView.frame.origin.y = 44
-            }
-        }
+    private func moveViewToRightPoint(duration: CGFloat = 1.0){
+        leftAnchor?.isActive = false
+        rightAnchor?.isActive = true
+        UIView.animate(withDuration: TimeInterval(duration), delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.autoreverse,.curveEaseOut], animations: {
+            self.view.layoutIfNeeded()
+        }, completion: {_ in
+            self.rightAnchor?.isActive = false
+            self.leftAnchor?.isActive = true
+        })
+    }
+    private func ViewToCircle(duration: CGFloat = 1.0){
+        let animation = CABasicAnimation(keyPath: "cornerRadius")
+        animation.fromValue = NSNumber(value: 0)
+        animation.toValue = NSNumber(value: Float(animatableView.frame.width/2))
+        animation.duration = TimeInterval(duration)
+        animation.autoreverses = true
+        animatableView.layer.add(animation,forKey: "cornerRadius")
+        
     }
     
+    private func rotateView(duration: CGFloat = 1.0,angle: Double = Double.pi) {
+            let rotation : CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation")
+            rotation.toValue = NSNumber(value: angle)
+            rotation.duration = TimeInterval(duration)
+            animatableView.layer.add(rotation, forKey: "transform.rotation")
+        }
+    private func viewDisappearance(duration: CGFloat = 1.0){
+        UIView.animate(withDuration: TimeInterval(duration), delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .autoreverse, animations: {
+            self.animatableView.alpha = 0
+        }, completion: { _ in
+            self.animatableView.alpha = 1.0
+        })
+    }
+    private func viewGrow(duration: CGFloat = 2.0){
+        leftAnchor?.isActive = false
+        rightAnchor?.isActive = false
+        topAnchor?.isActive = false
+        let centerXAnchor = animatableView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
+        centerXAnchor.isActive = true
+        let centerYAnchor = animatableView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        centerYAnchor.isActive = true
+        //widthAnchor?.isActive = false
+        //heightAnchor?.isActive = false
+        UIView.animate(withDuration: TimeInterval(duration), delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .autoreverse, animations: {
+            self.animatableView.transform = CGAffineTransform(scaleX: 2,y: 2)
+            self.view.layoutIfNeeded()
+        }, completion: { _ in
+            centerXAnchor.isActive = false
+            centerYAnchor.isActive = false
+            self.leftAnchor?.isActive = true
+            self.topAnchor?.isActive  = true
+            self.animatableView.transform = CGAffineTransform(scaleX: 1,y: 1)
+        })
+    }
+
+      private func infinityViewRotate(duration: Double = 1) {
+            if animatableView.layer.animation(forKey: kRotationAnimationKey) == nil {
+                let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+
+                rotationAnimation.fromValue = 0.0
+                rotationAnimation.toValue = Float.pi * 2.0
+                rotationAnimation.duration = duration
+                rotationAnimation.repeatCount = Float.infinity
+
+                animatableView.layer.add(rotationAnimation, forKey: kRotationAnimationKey)
+            }
+        }
+
+       private func stopInfinityRotating() {
+            if animatableView.layer.animation(forKey: kRotationAnimationKey) != nil {
+                animatableView.layer.removeAnimation(forKey: kRotationAnimationKey)
+            }
+        }
+    
+    @IBAction func startOrStopRotating(_ sender: UIButton) {
+        if(sender.titleLabel!.text == "Start Rotation"){
+            sender.setTitle("Stop Rotation", for: .normal)
+            infinityViewRotate()
+        }else{
+            sender.setTitle("Start Rotation", for: .normal)
+            stopInfinityRotating()
+        }
+        
+    }
 }
 
